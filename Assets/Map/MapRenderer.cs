eacedbf61536f83635a82a9980d322d846ec1b9b
hindr <hindrik1997@hotmail.com>
2017-02-09 16:57:29 +0100

    Implemented texture arrays, now use them in shader

--- Assets/Map/MapRenderer.cs
+++ Assets/Map/MapRenderer.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
 using Assets.Map.Generation;
 using Map;
-using Map.Generation;
 
 namespace Assets.Map
 {
@@ -12,11 +12,21 @@ namespace Assets.Map
         private ComputeBuffer computeBuffer;
         private HexBoard hexBoard;
 
-        public int MapSize = 1;
+        private Texture2DArray _albedoMaps;
+        private Texture2DArray _heightMaps;
+        private Texture2DArray _normalMaps;
+        private Texture2DArray _amboccMaps;
+        private Texture2DArray _glossyMaps;
+        private Texture2DArray _metallMaps;
+
+        private const int TextureSize = 1024;
+
+        public int MapSize;
+        private List<TextureSet> _textureSets;
+
         public Mesh Mesh;
 
         public Material HexMaterial;
-
 
         public Texture2D DefaultAlbedoMap;
         public Texture2D DefaultHeightMap;
@@ -26,14 +36,60 @@ namespace Assets.Map
         public Texture2D DefaultMetallMap;
 
 
-
+        private TextureSet _defaultTextureSet;
 
         [UsedImplicitly]
         private void Start()
         {
+            _defaultTextureSet = new TextureSet
+            {
+                DefaultAlbedoMap = DefaultAlbedoMap,
+                DefaultHeightMap = DefaultHeightMap,
+                DefaultAmbOccMap = DefaultAmbOccMap,
+                DefaultGlossyMap = DefaultGlossyMap,
+                DefaultMetallMap = DefaultMetallMap,
+                DefaultNormalMap = DefaultNormalMap
+            };
+
+            _textureSets = new List<TextureSet>();
+
+            for (var i = 0; i < Enum.GetNames(typeof(TileType)).Length; ++i)
+                _textureSets.Add(_defaultTextureSet);
+
+            _albedoMaps = new Texture2DArray(TextureSize, TextureSize, _textureSets.Count, TextureFormat.RGBA32, true);
+            _heightMaps = new Texture2DArray(TextureSize, TextureSize, _textureSets.Count, TextureFormat.Alpha8, true);
+            _normalMaps = new Texture2DArray(TextureSize, TextureSize, _textureSets.Count, TextureFormat.RGBA32, true);
+            _amboccMaps = new Texture2DArray(TextureSize, TextureSize, _textureSets.Count, TextureFormat.Alpha8, true);
+            _glossyMaps = new Texture2DArray(TextureSize, TextureSize, _textureSets.Count, TextureFormat.Alpha8, true);
+            _metallMaps = new Texture2DArray(TextureSize, TextureSize, _textureSets.Count, TextureFormat.Alpha8, true);
+
+            Debug.Log(Convert.ToInt32(Mathf.Log(1024,2)));
+
+            for (var i = 0; i < Enum.GetNames(typeof(TileType)).Length; ++i)
+            {
+                for (var j = 0; j < Convert.ToInt32(Mathf.Log(2,1024)); ++j)
+                {
+                    _albedoMaps.SetPixels(_textureSets[i].DefaultAlbedoMap.GetPixels(j), i , j);
+                    _heightMaps.SetPixels(_textureSets[i].DefaultHeightMap.GetPixels(j), i , j);
+                    _normalMaps.SetPixels(_textureSets[i].DefaultNormalMap.GetPixels(j), i , j);
+                    _amboccMaps.SetPixels(_textureSets[i].DefaultAmbOccMap.GetPixels(j), i , j);
+                    _glossyMaps.SetPixels(_textureSets[i].DefaultGlossyMap.GetPixels(j), i , j);
+                    _metallMaps.SetPixels(_textureSets[i].DefaultMetallMap.GetPixels(j), i , j);
+                }
+            }
+
+            _albedoMaps.Apply();
+            _amboccMaps.Apply();
+            _glossyMaps.Apply();
+            _heightMaps.Apply();
+            _metallMaps.Apply();
+            _normalMaps.Apply();
+
+
             hexBoard = new HexBoard(MapSize) {Generator = new TestGenerator()};
             hexBoard.GenerateMap();
 
+            /*
             CubicalCoordinate start = hexBoard.RandomValidTile();
 
             CubicalCoordinate goal = hexBoard.RandomValidTile();
@@ -51,7 +107,7 @@ namespace Assets.Map
 
             hexBoard[start] = (byte) TileType.Path;
             hexBoard[goal] = (byte) TileType.Path;
-
+            */
             SetupShader();
         }
 
@@ -75,11 +131,21 @@ namespace Assets.Map
             block.SetFloat(Shader.PropertyToID("_ArraySize"), MapSize);
             block.SetBuffer(Shader.PropertyToID("_HexagonBuffer"), computeBuffer);
 
+            block.SetTexture("_AlbedoMaps", _albedoMaps);
+            block.SetTexture("_NormalMaps", _normalMaps);
+            block.SetTexture("_HeightMaps", _heightMaps);
+            block.SetTexture("_AmbOccMaps", _amboccMaps);
+            block.SetTexture("_GlossyMaps", _glossyMaps);
+            block.SetTexture("_MetalMaps", _metallMaps);
+
+
             MeshFilter filter = gameObject.AddComponent<MeshFilter>();
             MeshRenderer mrenderer = gameObject.AddComponent<MeshRenderer>();
             filter.sharedMesh = Mesh;
             mrenderer.material = HexMaterial;
+
             mrenderer.SetPropertyBlock(block);
+
         }
 
         [UsedImplicitly]
---
+++ Assets/Map/TextureSet.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Map
+{
+    public class TextureSet
+    {
+        public Texture2D DefaultAlbedoMap { get; set; }
+        public Texture2D DefaultHeightMap { get; set; }
+        public Texture2D DefaultNormalMap { get; set; }
+        public Texture2D DefaultAmbOccMap { get; set; }
+        public Texture2D DefaultGlossyMap { get; set; }
+        public Texture2D DefaultMetallMap { get; set; }
+    }
+}
