8797f83b7f9f05bc3f94cb24e5b87697674a92d3
Ruurd Bijlsma <ruurdbijlsma@gmail.com>
2017-02-09 17:00:34 +0100

    added terraintype

---
+++ Assets/Map/Generation/TerrainType.cs
@@ -0,0 +1,17 @@
+﻿using Map;
+
+namespace Assets.Map.Generation
+{
+    internal class TerrainType
+    {
+        public TerrainType(TileType type, float height)
+        {
+            Type = type;
+            Height = height;
+        }
+
+        public TileType Type { get; }
+
+        public float Height { get; }
+    }
+}
