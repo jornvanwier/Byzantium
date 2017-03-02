7ff77d7a9d50e8e53cf3e13fcb8e33af31386ae6
Ruurd Bijlsma <ruurdbijlsma@gmail.com>
2017-03-02 14:26:43 +0100

    Replaced textures, updated perlingenerator

--- Assets/Map/Generation/PerlinGenerator.cs
+++ Assets/Map/Generation/PerlinGenerator.cs
@@ -65,7 +65,7 @@ namespace Assets.Map.Generation
 
                 bool[,] waterMap = Utils.LogOperationTime("Watermap", () => GetWaterMap(heightMap));
 
-                //             Utils.LogOperationTime("Rivers", () => AddRivers(waterMap, heightMap, 3));
+                Utils.LogOperationTime("Rivers", () => AddRivers(waterMap, heightMap, 3));
 
                 float[,] moistureMap = Utils.LogOperationTime("Moisturemap",
                     () => GetMoistureMap(waterMap, moistureResolution));
