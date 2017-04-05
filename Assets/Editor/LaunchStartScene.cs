3f94cb483138ff8026b025604c4e58c99a9cef06
Jorn van Wier <jornvanwier@gmail.com>
2017-04-05 17:19:37 +0200

    add loading screen (press ctrl+0 to see it)

---
+++ Assets/Editor/LaunchStartScene.cs
@@ -0,0 +1,26 @@
+﻿// class doesn't matter, add to anything in the Editor folder
+// for any beginners reading, this is c#
+
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Editor
+{
+    internal class LaunchStartScene
+    {
+        [MenuItem("Edit/Play-Stop, But From Prelaunch Scene %0")]
+        public static void PlayFromPrelaunchScene()
+        {
+            if (EditorApplication.isPlaying )
+            {
+                EditorApplication.isPlaying = false;
+                return;
+            }
+
+            EditorSceneManager.OpenScene("Assets/SceneLoader.unity");
+            EditorApplication.isPlaying = true;
+        }
+
+    }
+}
--- Assets/Scripts/UI/LoadingPanel.cs
+++ Assets/Scripts/UI/LoadingPanel.cs
@@ -1,39 +1,63 @@
 ﻿using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Timers;
+using System.Collections;
+using System.Text;
+using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.UI
 {
     public class LoadingPanel : MonoBehaviour
     {
-        private GameObject panel;
-        private GameObject faction1Text;
-        private GameObject faction2Text;
+        private Text faction1Text;
+        private Text faction2Text;
+        private Text statusText;
+
+        private string dots = ".";
+
+        private AsyncOperation loadProgress;
 
         // Use this for initialization
         [UsedImplicitly]
         private void Start()
         {
-            panel = GameObject.Find("LoadingPanel");
-            faction1Text = GameObject.Find("Faction1");
-            faction2Text = GameObject.Find("Faction2");
+            faction1Text = GameObject.Find("Faction1")?.GetComponent<Text>();
+            faction2Text = GameObject.Find("Faction2")?.GetComponent<Text>();
+            statusText = GameObject.Find("StatusText").GetComponent<Text>();
+
+            loadProgress = StartLoadGame();
         }
 
         // Update is called once per frame
         [UsedImplicitly]
         private void Update()
         {
-            faction1Text.GetComponent<Text>().text = "soep";
+            if (loadProgress.isDone)
+            {
+                SceneManager.SetActiveScene(SceneManager.GetSceneByName("Base"));
+            }
+            else
+            {
+                statusText.text = "Loading" + dots;
+
+                dots = dots.Length > 1 ? "." : dots += ".";
+            }
+        }
+
+        private static AsyncOperation StartLoadGame()
+        {
+            return SceneManager.LoadSceneAsync("Base");
         }
 
         public void Show()
         {
-            panel.SetActive(true);
+            gameObject.SetActive(true);
         }
 
         public void Hide()
         {
-            panel.SetActive(false);
+            gameObject.SetActive(false);
         }
     }
 }
