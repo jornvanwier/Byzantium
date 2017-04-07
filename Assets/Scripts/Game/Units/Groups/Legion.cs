0f5226b1e9b4445f76fb4b5406bf1432d3324ea8
Hindrik Stegenga <hindrik1997@hotmail.com>
2017-04-07 16:56:09 +0200

    fixje voor de merge

--- Assets/Scripts/Game/Units/Groups/Legion.cs
+++ Assets/Scripts/Game/Units/Groups/Legion.cs
@@ -76,6 +76,7 @@ namespace Assets.Scripts.Game.Units.Groups
         public void RemoveUnit(Cohort unit)
         {
             cohorts.Remove(unit);
+            set = MeshDrawableUnit.Prefetch(this);
         }
 
         IEnumerator IEnumerable.GetEnumerator()
